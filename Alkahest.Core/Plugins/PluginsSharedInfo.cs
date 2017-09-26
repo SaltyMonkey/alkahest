e545e8b7c6f42369f0947a7bff2b9bdc3a3aa0d8
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2017-09-26 19:58:41 +0300

    basic shared data for plugins
    PoC

---
+++ Alkahest.Core/Plugins/PluginsSharedInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alkahest.Core.Plugins
+{
+    public class PluginsSharedInfo
+    {
+        private static PluginsSharedInfo _instance;
+        public static PluginsSharedInfo Instance => _instance ?? (_instance = new PluginsSharedInfo());
+        public virtual string PluginsList { get; protected set; }
+        public PluginsSharedInfo()
+        {
+            
+        }
+    }
+
+
+}
