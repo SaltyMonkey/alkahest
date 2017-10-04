1a44edf7bda962cd9a3331e3a582d946d46d8cf7
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2017-10-04 18:00:05 +0300

    forgot about dir path

--- Alkahest.Core/Net/Protocol/OpCodes/OpCodeTable.cs
+++ Alkahest.Core/Net/Protocol/OpCodes/OpCodeTable.cs
@@ -35,7 +35,7 @@ namespace Alkahest.Core.Net.Protocol.OpCodes
             Version = version;
 
             //var asm = Assembly.GetExecutingAssembly();
-            var path = $@"{nameof(Net)}\{nameof(Protocol)}\{nameof(OpCodes)}\{(opCodes ? "opc" : "smt")}_{Version}.txt";
+            var path = $@"{AppDomain.CurrentDomain.BaseDirectory}\{nameof(Net)}\{nameof(Protocol)}\{nameof(OpCodes)}\{(opCodes ? "opc" : "smt")}_{Version}.txt";
             var codeToName = new Dictionary<ushort, string>();
             var nameToCode = new Dictionary<string, ushort>();
 
